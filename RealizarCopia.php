3a82fbbee9641cbdb4bb60ddf701b84e90aa515e
ASIR <ASIR@PC5053>
2018-12-18 13:02:21 +0100

    Es nuestro tercer commit

--- RealizarCopia.php
+++ RealizarCopia.php
@@ -1,8 +1,9 @@
 <?Php
 include_once 'head.php';
+echo"<h2>este formulario es para hacer copias de seguridad</h2>";
 echo'<form action="RealizarCopia.php" method="POST">
 <input type="radio" name="rbCopia" value="0" />Solo una base de datos<br>
-<input type="radio" name="rbCopia" value="0" /> Todas las bases de datos<br>
+<input type="radio" name="rbCopia" value="0" /> Todas las bases de datos<br><input type="text" name="nombrecopia" value="" />
 <input type="submit" value="Realizar" /><br>
 </form>
 </body>'
